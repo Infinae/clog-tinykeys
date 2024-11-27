77ed61044dfb5bb1a92ba8a0ca63917541872c2e
infinae <infinae@infinae.io>
2024-11-27 15:01:32 +1100

    Only dispatch when event target and listener match
    By default custom events will only be triggered when the listener and
    the event target match so as to 'disable' keybinding when child elements
    have focus. This will make typing into text inputs and other content
    editable elements work as expected. Specify :always-active T or NIL to
    explicitly turn this behavior on or off, either for all keybindings or
    on an individual basis.

--- clog-tinykeys.lisp
+++ clog-tinykeys.lisp
@@ -6,12 +6,17 @@
 (cl:in-package :clog-tinykeys)
 
 (defparameter *tinykeys-format-string*
-  "~
-tinykeys.tinykeys(~~\0@*~~\A.get().at(0),{~
-~{~{'~A': (event) => {~
-~:[~; event.preventDefault();~]~
-~:[~; event.stopPropagation();~&~]~
-~~\0@*~~\A.trigger('~~\1@*~~\A', '~A');},~}~}})")
+  "
+tinykeys.tinykeys(~~\0@*~~\A.get().at(0), { ~
+~{~{
+    '~A': (event) => {
+        if (~:[false~;true~] || event.target === ~~\0@*~~\A.get().at(0)) {
+~:[~;            event.preventDefault()~&~]~
+~:[~;            event.stopPropagation()~&~]~:
+            ~~\0@*~~\A.trigger('~~\1@*~~\A', '~A')
+        }
+    },~}~}
+})")
 
 (defun load-tinykeys (document)
   (clog:load-script document "https://cdn.jsdelivr.net/npm/tinykeys@3.0.0/dist/tinykeys.umd.min.js"))
@@ -31,30 +36,36 @@ tinykeys.tinykeys(~~\0@*~~\A.get().at(0),{~
                                  (funcall handler))
                                nil))))))
 
-(defun generate-script (plists)
+
+
+(defun generate-script (plists always-active)
   (loop for i from 0
         for plist in plists
         for id = (format nil "TK~D" i)
         for keys = (getf plist :keys)
         for prevent-default = (getf plist :prevent-default)
         for stop-propagation = (getf plist :stop-propagation)
-        collect (list keys prevent-default stop-propagation id) into parameters
+        for always-active* = (if (get-properties plist '(:always-active))
+                                (getf plist :always-active)
+                                always-active)
+        collect (list keys always-active* prevent-default stop-propagation id) into parameters
         finally (return (format nil *tinykeys-format-string* parameters))))
 
-(defun generate-condition-clause (clause clog-obj)
+(defun generate-condition-clause (clause clog-obj always-active)
   (let ((condition (first clause))
         (event-handler (generate-event-handler (rest clause)))
-        (script (generate-script (rest clause))))
+        (script (generate-script (rest clause) always-active))
+        (js-always-active (if always-active "true" "false")))
     `(when ,condition
        (let ((event-name (format nil "TKS~D" (clog:generate-id)))
              (event-handler ,event-handler)
              (script ,script))
-         (clog:set-on-event-with-data ,clog-obj event-name event-handler :cancel-event t)
+         (clog:set-on-event-with-data (clog:connection-body ,clog-obj) event-name event-handler :cancel-event t)
          (clog:js-execute ,clog-obj (format nil script (clog:jquery ,clog-obj) event-name))))))
 
-(defmacro set-on-keys (clog-obj &body clauses)
+(defmacro set-on-keys ((clog-obj &key (always-active nil)) &body clauses)
   (if (plusp (length clauses))
       `(progn ,@(loop for clause in clauses
-                      collect (generate-condition-clause clause clog-obj) into code
+                      collect (generate-condition-clause clause clog-obj always-active) into code
                       finally (return code)))
       nil))
