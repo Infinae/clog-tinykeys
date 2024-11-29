b478b0e713c5cd4ccaf2b00758934bb2ddfeb8b4
infinae <infinae@infinae.io>
2024-11-29 19:40:19 +1100

    Revamp set-on-keys syntax & trigger conditions
    The new syntax does away with some of the keyword arguments and
    structures the keybinding definition so as to allow for an implicit
    lambda to wrap the body of the handler. It introduces the :trigger
    keyword argument to explicitly change the trigger condition.
    
    Initially, no trigger condition was implemented which meant that
    listeners were triggering handlers freely as the keys matched with
    propagated events. This was a problem because it interfered with the
    behavior of elements such as text inputs.
    
    A default condition for triggering handlers when keys were pressed was
    introduced to ensure that the event target and listener matched, but
    this was found to be slightly too restrictive. This did enable text
    inputs to behave as expected, but didn't allow keybindings to have scope
    or be 'inherited'.
    
    The user may want to define some global keybindings, but also they may
    want certain keybindings to become active when certain parts of the
    document have 'focus'. This requires some form of scoping and
    inheritance. One, albeit hacky, way to achieve scoping is by setting
    intermediate child DIVs with a tabIndex of -1 so that they can receive
    keyboard events. That way when they have focus, and events arrive via
    propagation, they have the chance to trigger the appropriate handlers.
    Unfortunately enforcing the condition that event target and listener
    must match doesn't allow for the global keybindings to operate since it
    is the intermediate DIV that has the focus.
    
    This commit opens up the default condition so that key matched events
    freely trigger handers again, but only if the activeElement isn't an
    input element, with the exception of the event target also being the
    listener (so that if the input element is the listener it doesn't
    exclude itself).

--- clog-tinykeys.lisp
+++ clog-tinykeys.lisp
@@ -7,11 +7,15 @@
 
 (defparameter *tinykeys-format-string*
   "
-clogTKSU['~~\1@*~~\A'] = (
+clogTinykeysUnsubscribers['~~\1@*~~\A'] = (
     tinykeys.tinykeys(~~\0@*~~\A.get().at(0), { ~
 ~{~{
         '~A': (event) => {
-            if (~:[false~;true~] || event.target === ~~\0@*~~\A.get().at(0)) {
+            if (~[~
+                      (event.target === ~~\0@*~~\A.get().at(0)) || $(document.activeElement).is(':not(input)')~
+                      ~;true~
+                      ~;event.target === ~~\0@*~~\A.get().at(0)~
+             ~]) {
 ~:[~;                event.preventDefault()~&~]~
 ~:[~;                event.stopPropagation()~&~]~:
                 ~~\0@*~~\A.trigger('~~\1@*~~\A', '~A')
@@ -22,13 +26,18 @@ clogTKSU['~~\1@*~~\A'] = (
 
 (defun load-tinykeys (document)
   (clog:load-script document "https://cdn.jsdelivr.net/npm/tinykeys@3.0.0/dist/tinykeys.umd.min.js")
-  (clog:js-execute document "clogTKSU = {}"))
+  (clog:js-execute document "clogTinykeysUnsubscribers = {}"))
 
 (defun generate-event-handler (plists)
   (loop for i from 0
         for plist in plists
         for id = (format nil "TK:~D" i)
-        for handler = (getf plist :handler)
+        for handler = (let ((handler-body (getf plist :handler-body)))
+                        (cond ((null handler-body) '(lambda () nil))
+                              ((and (= (length handler-body) 1)
+                                    (search "#'" (write-to-string (first handler-body))))
+                               `,(first handler-body))
+                              (t `(lambda () ,@handler-body))))
         collect `(setf (gethash ,id handlers) ,handler) into setfs
         finally (return `(let ((handlers (clog:make-hash-table* :test 'equal)))
                            ,@setfs
@@ -39,45 +48,87 @@ clogTKSU['~~\1@*~~\A'] = (
                                  (funcall handler))
                                nil))))))
 
-(defun generate-script (plists always-active)
+(defun generate-script (plists)
   (loop for i from 0
         for plist in plists
         for id = (format nil "TK:~D" i)
         for keys = (getf plist :keys)
-        for prevent-default = (getf plist :prevent-default)
+        for trigger = (let ((value (getf plist :trigger)))
+                        (cond ((eq value :default) 0)
+                              ((eq value :always) 1)
+                              ((eq value :is-target) 2)))
         for stop-propagation = (getf plist :stop-propagation)
-        for always-active* = (if (get-properties plist '(:always-active))
-                                (getf plist :always-active)
-                                always-active)
-        collect (list keys always-active* prevent-default stop-propagation id) into parameters
+        for prevent-default = (getf plist :prevent-default)
+        collect (list keys trigger prevent-default stop-propagation id) into parameters
         finally (return (format nil *tinykeys-format-string* parameters))))
 
-(defun tks-id-p (string)
-  (let ((tmp (search "TKS:" string)))
-    (if tmp (if (zerop tmp) t nil) nil)))
-
 (defun normalise-id (id)
-  (if id
-      (if (tks-id-p id) id (format nil "TKS:~A" id))
-      (format nil "TKS:~A" (clog:generate-id))))
+  (assert (or (stringp id) (and (keywordp id) (eq id :undefined))))
+  (if (eq id :undefined)
+      (format nil "TKS:~A" (clog:generate-id))
+      (if (not (zerop (or (search "TKS:" id) -1)))
+          (format nil "TKS:~A" id)
+          id)))
 
-(defun generate-keybindings (keybindings clog-obj id always-active)
-  (let ((event-handler (generate-event-handler keybindings))
-        (script (generate-script keybindings always-active)))
+(defun restructure-keybinding (keybinding)
+  (destructuring-bind
+      ((keys &key (stop-propagation :undefined) (prevent-default :undefined) (trigger :undefined)) &rest handler-body) keybinding
+    (list :keys keys
+          :trigger trigger
+          :stop-propagation stop-propagation
+          :prevent-default prevent-default
+          :handler-body handler-body)))
+
+(defun merge-value (keyword outer-value inner-value)
+  (cond ((and (eq outer-value :undefined) (eq inner-value :undefined))
+         (cond ((eq keyword :trigger) :default)
+               ((eq keyword :stop-propagation) nil)
+               ((eq keyword :prevent-default) nil)))
+        ((eq inner-value :undefined) outer-value)
+        (t inner-value)))
+
+(defun merge-properties (outer-properties inner-properties)
+  (loop for keyword in '(:trigger :stop-propagation :prevent-default)
+        for outer-value = (getf outer-properties keyword)
+        for inner-value = (getf inner-properties keyword)
+        nconc (list keyword (merge-value keyword outer-value inner-value)) into result
+        finally (return
+                  (concatenate
+                   'list
+                   (list :keys (getf inner-properties :keys))
+                   (list :handler-body (getf inner-properties :handler-body))
+                   result))))
+
+(defun generate-keybindings (clog-obj id parameters keybindings)
+  (assert (not (null keybindings)) nil "Please enter NIL or one or more keybinding forms.")
+  (let* ((keybindings (loop for kb in keybindings collect (restructure-keybinding kb)))
+         (keybindings (loop for kb in keybindings collect (merge-properties parameters kb)))
+         (event-handler (generate-event-handler keybindings))
+         (script (generate-script keybindings)))
     (when (or id keybindings)
       `(let ((event-name (normalise-id ,id))
              ,@(when keybindings
-                `((event-handler ,event-handler)
-                  (script ,script))))
-         ,@(when id
+                 `((event-handler ,event-handler)
+                   (script ,script))))
+         ,@(unless (or (not id) (keywordp id) (eq id :undefined))
              `((clog:set-on-event-with-data (clog:connection-body ,clog-obj) event-name nil)
-               (clog:js-execute ,clog-obj (format nil "clogTKSU['~@*~A']?.(); delete clogTKSU['~@*~A'];" event-name))))
+               (clog:js-execute ,clog-obj (format nil "clogTinykeysUnsubscribers['~@*~A']?.(); delete clogTinykeysUnsubscribers['~@*~A'];" event-name))))
          ,@(when keybindings
              `((clog:set-on-event-with-data (clog:connection-body ,clog-obj) event-name event-handler :cancel-event t)
                (clog:js-execute ,clog-obj (format nil script (clog:jquery ,clog-obj) event-name))))
          event-name))))
 
-(defmacro set-on-keys ((clog-obj &key (id nil) (always-active nil)) &body keybindings)
-  (when (and (= (length keybindings) 1) (eq (first keybindings) nil))
-    (setf keybindings nil))
-  (generate-keybindings keybindings clog-obj id always-active))
+(defmacro set-on-keys
+    ((clog-obj
+      &key
+        (id :undefined)
+        (trigger :undefined)
+        (stop-propagation :undefined)
+        (prevent-default :undefined))
+     &body keybindings)
+  (generate-keybindings
+   clog-obj id
+   (list :trigger trigger
+         :stop-propagation stop-propagation
+         :prevent-default prevent-default)
+   keybindings))
